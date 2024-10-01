cf7f93e3756bd4c53a2dba92a69201547882a49a
Sasaki-Ren07 <ren_sasaki@mail.toyota.co.jp>
2024-10-02 07:18:29 +0900

    Create questionList.js

---
+++ questionList.js
@@ -0,0 +1,69 @@
+`use strict`;
+//question1
+const question1 = {
+    word : "function addTen(number) {\n  return number + 10;\n}",
+    supplement : "function  関数宣言時に使用する文言",
+    supplement2 : "return  関数の実行を終了し、関数の呼び出しもとに返す値を指定します",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function",
+    link2 : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function"
+}
+
+
+const question2 = {
+    word : "function",
+    supplement : "関数宣言時に使用する文言",
+    supplement2 : "",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function",
+    link2 : ""
+};
+
+const question3 = {
+    word : "return",
+    supplement : "関数の実行を終了し、関数の呼び出しもとに返す値を指定します",
+    supplement2 : "",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/return",
+    link2 : ""
+};
+
+const question4 = {
+    word : "console.log()",
+    supplement : "メッセージをウェブコンソールに出力します。consoleオブジェクトの中のメソッドの1つです。",
+    supplement2 : "consoleオブジェクトについてはこちら！",
+    link : "https://developer.mozilla.org/ja/docs/Web/API/console/log_static",
+    link2 : "https://developer.mozilla.org/ja/docs/Web/API/console"
+};
+
+const question5 = {
+    word : "Math.random()",
+    supplement : "0以上1未満の値をランダムに返します。Mathオブジェクトの中のメソッドの1つです。",
+    supplement2 : "Mathオブジェクトについてはこちら！",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random",
+    link2 : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math"
+};
+
+
+const question6 = {
+    word : "function biggerNumber(numA, numB) {\n  if (numA > numB) {\n    return \"A is bigger\";\n  }",
+    supplement : "if...else 条件分岐させるのに使用する文",
+    supplement2 : "",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/if...else",
+    link2 : ""
+};
+
+
+const question7 = {
+    word : "function sayIfComfortable(temp) {\n  if (temp >= 20 && temp <= 25) {\n    return \"It's not comfortable.\";\n  }",
+    supplement : "if...else 条件分岐させるのに使用する文",
+    supplement2 : "&&  論理積(A かつ B)  || 論理和(A または B)",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_AND",
+    link2 : ""
+};
+
+
+const question8 = {
+    word : "const info = [9, true, \"Hello\", 201]",
+    supplement : "[] 配列要素 複数の値を1つの変数に保存できる",
+    supplement2 : "",
+    link : "https://developer.mozilla.org/ja/docs/Learn/JavaScript/First_steps/Arrays",
+    link2 : ""
+};
