0b1612ed77b5996bb94e3f522c0911af375833f1
Sasaki-Ren07 <ren_sasaki@mail.toyota.co.jp>
2024-10-02 17:48:27 +0900

    Update questionList.js

--- questionList.js
+++ questionList.js
@@ -2,7 +2,7 @@
 //question1
 const question1 = {
     word : "function addTen(number) {\n  return number + 10;\n}",
-    supplement : "function  関数宣言時に使用する文言",
+    supplement : "function  関数宣言時に使用する文言です。",
     supplement2 : "return  関数の実行を終了し、関数の呼び出しもとに返す値を指定します",
     link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function",
     link2 : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function"
@@ -11,7 +11,7 @@ const question1 = {
 
 const question2 = {
     word : "function",
-    supplement : "関数宣言時に使用する文言",
+    supplement : "関数宣言時に使用する文言です。",
     supplement2 : "",
     link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function",
     link2 : ""
@@ -44,7 +44,7 @@ const question5 = {
 
 const question6 = {
     word : "function biggerNumber(numA, numB) {\n  if (numA > numB) {\n    return \"A is bigger\";\n  }",
-    supplement : "if...else 条件分岐させるのに使用する文",
+    supplement : "if...else 条件分岐させるのに使用する文言です。",
     supplement2 : "",
     link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/if...else",
     link2 : ""
@@ -53,7 +53,7 @@ const question6 = {
 
 const question7 = {
     word : "function sayIfComfortable(temp) {\n  if (temp >= 20 && temp <= 25) {\n    return \"It's not comfortable.\";\n  }",
-    supplement : "if...else 条件分岐させるのに使用する文",
+    supplement : "if...else 条件分岐させるのに使用する文言です。",
     supplement2 : "&&  論理積(A かつ B)  || 論理和(A または B)",
     link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_AND",
     link2 : ""
@@ -66,4 +66,31 @@ const question8 = {
     supplement2 : "",
     link : "https://developer.mozilla.org/ja/docs/Learn/JavaScript/First_steps/Arrays",
     link2 : ""
+};
+
+
+const question9 = {
+    word : "const school = \"DIG college\";",
+    supplement : "const 変数を宣言する文言 letと違い再代入できません。",
+    supplement2 : "= 代入演算子 変数に値を代入するのに使用します。数学的なイコールじゃないよ！",
+    link : "https://developer.mozilla.org/ja/docs/Learn/JavaScript/First_steps/Arrays",
+    link2 : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Assignment"
+};
+
+
+const question10 = {
+    word : "typeof",
+    supplement : "変数のデータ型を文字列で返す演算子です。 ",
+    supplement2 : "",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/typeof",
+    link2 : ""
+};
+
+
+const question11 = {
+    word : "Array.length",
+    supplement : "arrayがもつプロパティで配列の要素数を表します。",
+    supplement2 : "Stringの方でも同名のプロパティがあり、こちらは文字列の長さを表します。",
+    link : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/length",
+    link2 : "https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/length"
 };
