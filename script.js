aedd3e816a496b8e8e2c17783a3de1fd77252bdb
Sasaki-Ren07 <ren_sasaki@mail.toyota.co.jp>
2024-10-03 08:59:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 //問題文リスト
 const questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, 
-    question11];
+    question11, question12];
 const numOfQuestions = questions.length;
 
 
